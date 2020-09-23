bba08b71ad9f4de7a5a1ed6301474b7e6a01b275
Victor Ângelo Sánchez da Silva <53021597+victorangelov@users.noreply.github.com>
2020-09-23 20:33:37 -0300

    commit de quase todo o projeto rsr
    commit de quase todo o projeto rsr

--- script/script.js
+++ script/script.js
@@ -1,1 +1,55 @@
-//alert('Olá! mather fucker')
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.querySelector('input#txtano')
+    var res = document.querySelector('div#res')
+
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERRO] - verifique os dados e tente novamente')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'homem'
+            if(idade > 0 && idade < 11){
+                //criança
+                img.setAttribute('src','img/menino.jpeg')
+            } else if(idade < 18){
+                //adolecente
+                img.setAttribute('src','img/jovemh.jpeg')
+            } else if(idade < 59){
+                //adulto
+                img.setAttribute('src','img/adulto.jpeg')
+            }else{
+                //velho
+                img.setAttribute('src','img/velho.jpeg')
+            }
+        } else if(fsex[1].checked){
+            genero = 'mulher'
+            if(idade > 0 && idade < 11){
+                //criança
+                img.setAttribute('src','img/menina.jpeg')
+            } else if(idade < 18){
+                //adolecente
+                img.setAttribute('src','img/jovemm.jpeg')
+            } else if(idade < 59){
+                //adulto
+                img.setAttribute('src','img/adulta.jpeg')
+            }else{
+                //velho
+                img.setAttribute('src','img/velha.jpeg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+
+    }
+    
+   
+
+     
+}
